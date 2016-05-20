0e5d5abb475a729c8a8e22d05f8f374a1c3a061e
fusobotic <fusionstudio3d@gmail.com>
2016-05-19 23:37:12 -0400

    added nav highlighting and social icons
    also added coppoletti title with proper formatting

--- gulpfile.js
+++ gulpfile.js
@@ -58,7 +58,7 @@ gulp.task('sass', function () {
 gulp.task('watch', function () {
     gulp.watch('_scss/*.scss', ['sass']);
     gulp.watch('_scss/*.sass', ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_includes/*', 'assets/*', '_posts/*'], ['jekyll-rebuild']);
+    gulp.watch(['*.html', '*.md', '_layouts/*.html', '_includes/*', 'assets/*', '_posts/*'], ['jekyll-rebuild']);
 });
 
 /**
